2db36309e3873bd140849519aab1e6a5232a1407
Laura Colín <lcolin01@gmail.com>
2025-07-12 11:31:46 -0600

    modificando Proyecto frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css"; // Asegúrate de tener este archivo creado
 
 function App() {
   const [message, setMessage] = useState("");
