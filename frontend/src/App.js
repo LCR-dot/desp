df414d55aaec0579ada081d6d67c3e926e524f7f
Laura Colín <lcolin01@gmail.com>
2025-07-12 11:22:14 -0600

    modificando Proyecto frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
 <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300">
   <div className="bg-white p-8 rounded-2xl shadow-2xl max-w-md text-center">
-    <h1 className="text-3xl font-bold text-blue-600 mb-4">Página de Presencia modificada</h1>
+    <h1 className="text-3xl font-bold text-blue-600 mb-4">Página de Presencia modificada GIT</h1>
     <p className="text-gray-700 text-lg">{message}</p>
   </div>
 </div>
